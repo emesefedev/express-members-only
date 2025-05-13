ebfc7be79412281812cd6cd927d03305c6724986
emesefedev <emesefedev@gmail.com>
2025-05-13 17:25:36 +0200

    docs: create custom error

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"))
 
+app.use((err, req, res, next) => {
+  console.error(err)
+  res.status(err.statusCode || 500).send(err.message)
+});
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`)
-});
+})
