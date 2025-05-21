11f13cf4980f3f9f138c52504ea00a35d4ae3c38
emesefedev <emesefedev@gmail.com>
2025-05-21 15:21:48 +0200

    refactor: app.js file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const session = require("express-session")
 const passport = require("passport")
-const bcryptjs = require("bcryptjs")
 
 const PGStore = require('connect-pg-simple')(session)
 
 const pool = require("./db/pool")
 const db = require("./db/queries")
+const routes = require("./routes")
 
 // ----- GENERAL SETUP -----
 
@@ -17,6 +17,12 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// ----- VIEWS -----
+
+const path = require("node:path")
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
 // ----- SESSION SETUP ------
 
@@ -35,23 +41,13 @@ app.use(session({  // Session has data stored in the server
   }
 }))
 
+// ----- PASSPORT AUTHENTICATION ------
 
-const LocalStrategy = require('passport-local').Strategy
+require("./config/passport")
 
-
-
-
-
-
-
-// VIEWS
-const path = require("node:path")
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
-
-
-
+app.use(passport.initialize())
 app.use(passport.session())
+
 
 
 
@@ -60,56 +56,11 @@ app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).send(err.message)
 })
 
-// PASSPORT
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await db.getUserFromUsername(username)
+// ----- ROUTES ------
 
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" })
-      }
+app.use(routes) // TODO: Comprobar que funciona
 
-      const match = await bcryptjs.compare(password, user.password)
-      if (!match) {
-        return done(null, false, { message: "Incorrect password" })
-      }
-      return done(null, user)
-    } catch(error) {
-      return done(error)
-    }
-  })
-)
-
-passport.serializeUser((user, done) => {
-  done(null, user.id)
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await db.getUserFromID(id)
-    done(null, user)
-  } catch(err) {
-    done(err)
-  }
-})
-
-// ROUTES
-const signUpRouter = require("./routes/sign-up-router")
-const logInRouter = require("./routes/log-in-router")
-const logOutRouter = require("./routes/log-out-router")
-const messagesRouter = require("./routes/messages-router")
-
-app.use("/sign-up", signUpRouter)
-app.use("/log-in", logInRouter)
-app.use("/log-out", logOutRouter)
-app.use("/messages", messagesRouter)
-
-
-app.get("/", (req, res) => {
-    res.render("index", { title: "Home" })
-})
-
+// ----- SERVER ------
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
