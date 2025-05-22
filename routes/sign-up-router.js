96964731651899b7655e2851f477df1507ba8721
emesefedev <emesefedev@gmail.com>
2025-05-22 15:33:04 +0200

    feat: sign up validations

--- routes/sign-up-router.js
+++ routes/sign-up-router.js
@@ -1,22 +1,24 @@
 const { Router } = require("express")
 const db = require("../db/queries")
 const { generatePassword } = require("../utilities/password")
+const { validationResult } = require("express-validator")
+const { validateUser } = require("../controllers/usersController")
 
 const signUpRouter = Router()
 
 signUpRouter.get("/", async (req, res) => {
-  
   res.render("sign-up", { title: "Sign Up" })
 })
 
-signUpRouter.post("/", async (req, res, next) => {
-  try {
-    const { salt, hash } = generatePassword(req.body.password)
-    await db.insertUser(req.body.username, req.body.first_name, req.body.last_name, hash, salt)
-    res.redirect("/log-in")
-  } catch(err) {
-    return next(err) // ¿Se me quedará pillada porque el error se gestiona en app.js?
+signUpRouter.post("/", [validateUser, async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).render("sign-up", { title: "Sign Up", errors: errors.array() })
   }
-})
+
+  const { salt, hash } = generatePassword(req.body.password)
+  await db.insertUser(req.body.username, req.body.first_name, req.body.last_name, hash, salt)
+  res.redirect("/log-in")
+}])
 
 module.exports = signUpRouter
