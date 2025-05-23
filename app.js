75194f6450edaa4530724d8b828795c5ee9f41e8
emesefedev <emesefedev@gmail.com>
2025-05-23 17:10:01 +0200

    install and set up tailwind

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ const path = require("node:path")
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath))
+
 // ----- SESSION SETUP ------
 
 // We need to create the "session" table in our database. psql <mydatabase> < node_modules/connect-pg-simple/table.sql
