051114a72cda2618b976219dfb384096378288c5
emesefedev <emesefedev@gmail.com>
2025-05-26 20:00:29 +0200

    views: style messages page

--- routes/messages-router.js
+++ routes/messages-router.js
@@ -1,10 +1,11 @@
 const { Router } = require("express")
 const { isAuthenticated } = require("../utilities/authMiddleware") 
+const { capitalize } = require("../utilities/strings")
 
 const messagesRouter = Router()
 
 messagesRouter.get("/", isAuthenticated, (req, res ) => {
-  res.render("messages", { title: "Messages", user: req.user })
+  res.render("messages", { title: "Messages", user: req.user, capitalize })
 })
 
 module.exports = messagesRouter
